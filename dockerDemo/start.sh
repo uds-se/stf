49dbe6119f8c958021fcc0557732cc9dcf6e507f
Timo Gühring <timo.guehring@googlemail.com>
2019-04-23 17:03:34 +0200

    Fix start.sh arguments

--- dockerDemo/start.sh
+++ dockerDemo/start.sh
@@ -1,1 +1,1 @@
-stf local --public-ip localhost:8080 auth-type mock --group-timeout 18000 --auth-token "44f8ae7a8c284660b9eb33b4cad94dea5e5c14f159054986be70ebb4d2a09eba" --adb-keys-dir-host "~/adbkeys/"
+stf local --auth-type mock --group-timeout 18000 --auth-token "44f8ae7a8c284660b9eb33b4cad94dea5e5c14f159054986be70ebb4d2a09eba" --adb-keys-dir-host "~/adbkeys/"
