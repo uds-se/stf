995389855c5ec32724e7ea4f941033455bb9bd60
Timo Gühring <timo.guehring@googlemail.com>
2019-04-23 16:16:09 +0200

    Introduce lst label

--- dockerDemo/runDocker.sh
+++ dockerDemo/runDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_IMAGE="stfdemo"
+DOCKER_IMAGE="stfdemo:lst"
 ADB_PATH_HOST="/opt/Android/Sdk/platform-tools/adb"
 ADB_PATH_CONTAINER="/usr/local/bin/adb"
 
@@ -10,6 +10,8 @@ docker build -t ${DOCKER_IMAGE} .
 #docker run -it --entrypoint "/bin/bash" ${DOCKER_IMAGE}
 docker run --net=host \
     --privileged \
+    # Mount the host docker socket
+    # https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ${ADB_PATH_HOST}:${ADB_PATH_CONTAINER} \
     ${DOCKER_IMAGE}
--- dockerDemo/startFarm.sh
+++ dockerDemo/startFarm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Check for Docker
 docker --version;
 # Give the database time to start
 rethinkdb & (sleep 10 && /stf/dockerDemo/start.sh)
