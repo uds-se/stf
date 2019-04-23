a0b2f96e7b01997d2a37847906494eadedacb6d8
Timo Gühring <timo.guehring@googlemail.com>
2019-04-23 15:32:29 +0200

    Update

--- dockerDemo/runDocker.sh
+++ dockerDemo/runDocker.sh
@@ -10,5 +10,6 @@ docker build -t ${DOCKER_IMAGE} .
 #docker run -it --entrypoint "/bin/bash" ${DOCKER_IMAGE}
 docker run --net=host \
     --privileged \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v ${ADB_PATH_HOST}:${ADB_PATH_CONTAINER} \
     ${DOCKER_IMAGE}
--- dockerDemo/startFarm.sh
+++ dockerDemo/startFarm.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+
+docker --version;
 # Give the database time to start
 rethinkdb & (sleep 10 && /stf/dockerDemo/start.sh)
