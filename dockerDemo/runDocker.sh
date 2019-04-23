2fe4fc7223ab334737deda18fcf074108dbd5c3e
Timo Gühring <timo.guehring@googlemail.com>
2019-04-23 16:46:24 +0200

    Fix npm install, fix runDocker.sh

--- dockerDemo/runDocker.sh
+++ dockerDemo/runDocker.sh
@@ -7,11 +7,11 @@ ADB_PATH_CONTAINER="/usr/local/bin/adb"
 docker build -t ${DOCKER_IMAGE} .
 
 # Interactive run
+# Mount the host docker socket
+# https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
 #docker run -it --entrypoint "/bin/bash" ${DOCKER_IMAGE}
 docker run --net=host \
     --privileged \
-    # Mount the host docker socket
-    # https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ${ADB_PATH_HOST}:${ADB_PATH_CONTAINER} \
     ${DOCKER_IMAGE}
--- dockerDemo/startFarm.sh
+++ dockerDemo/startFarm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Check for Docker
-docker --version;
+docker ps;
 # Give the database time to start
 rethinkdb & (sleep 10 && /stf/dockerDemo/start.sh)
