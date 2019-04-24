a91a3d3f6a6b5b0ab9431b30ce56feb97e1eca13
Timo Gühring <timo.guehring@googlemail.com>
2019-04-24 08:16:46 +0200

    Fix npm setup

--- dockerDemo/runDocker.sh
+++ dockerDemo/runDocker.sh
@@ -3,15 +3,14 @@
 DOCKER_IMAGE="stfdemo:lst"
 ADB_PATH_HOST="/opt/Android/Sdk/platform-tools/adb"
 ADB_PATH_CONTAINER="/usr/local/bin/adb"
-
-docker build -t ${DOCKER_IMAGE} .
-
-# Interactive run
 # Mount the host docker socket
 # https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
-#docker run -it --entrypoint "/bin/bash" ${DOCKER_IMAGE}
-docker run --net=host \
-    --privileged \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v ${ADB_PATH_HOST}:${ADB_PATH_CONTAINER} \
-    ${DOCKER_IMAGE}
+DOCKER_PARAMETERS="--net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${ADB_PATH_HOST}:${ADB_PATH_CONTAINER}"
+
+if docker build -t ${DOCKER_IMAGE} . ; then
+
+        # Interactive run
+        #docker run -it ${DOCKER_PARAMETERS} --entrypoint "/bin/bash" ${DOCKER_IMAGE}
+        #docker run -it --entrypoint "/bin/bash" ${DOCKER_IMAGE}
+        docker run ${DOCKER_PARAMETERS} --rm ${DOCKER_IMAGE}
+fi
